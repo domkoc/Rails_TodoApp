81201b56fff6965e9297a2bca39a354f83ca4d6e
Dominik Kocka <Dominik.Kocka@fodmobilitygroup.com>
2022-10-29 16:18:45 +0200

    Fix done todos route and add index redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :todos
+  resources :todos do
+    get 'done', on: :collection
+  end
+
   get 'users/new'
   post 'users/create'
+  root 'home#login'
   get 'home/login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
